bc0c93fa88697aba0dfd3ca0c0365864fd700b45
Yuuggyy <yuuggyy@users.noreply.github.com>
2026-08-29 09:36:05 +0000

    Debug: add AuthProvider back

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { AuthProvider } from "./lib/auth";
 import LandingPageMinimal from "./routes/LandingPageMinimal";
 import AppErrorBoundary from "./components/AppErrorBoundary";
 
 export default function App() {
   return (
     <AppErrorBoundary>
-      <BrowserRouter basename="/inspire-by-yuustore">
-        <Routes>
-          <Route path="/" element={<LandingPageMinimal />} />
-          <Route path="*" element={<Navigate to="/" />} />
-        </Routes>
-      </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter basename="/inspire-by-yuustore">
+          <Routes>
+            <Route path="/" element={<LandingPageMinimal />} />
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
     </AppErrorBoundary>
   );
 }
